a700a65803658a84b7ce5b9072107a68342734f2
temeddix <temeddix@gmail.com>
2022-03-11 19:13:56 +0900

    Change leverage settings file to mode settings file

--- module/recipe/examine_data_files.py
+++ module/recipe/examine_data_files.py
@@ -1,6 +1,7 @@
 import pickle
 import copy
 import os
+import json
 
 
 def do(datapath):
@@ -12,5 +13,20 @@ def do(datapath):
             realtime_data_chunks = copy.deepcopy(pickle.load(file))
         if isinstance(realtime_data_chunks, list):
             os.remove(filepath)
+    except FileNotFoundError:
+        pass
+
+    # 2.0: renamed leverage_settings to mode_settings
+    try:
+        filepath = datapath + "/transactor/leverage_settings.json"
+        with open(filepath, "r", encoding="utf8") as file:
+            desired_leverage = json.load(file)["desired_leverage"]
+        os.remove(filepath)
+        mode_settings = {
+            "desired_leverage": desired_leverage,
+        }
+        filepath = datapath + "/transactor/mode_settings.json"
+        with open(filepath, "w", encoding="utf8") as file:
+            json.dump(mode_settings, file, indent=4)
     except FileNotFoundError:
         pass
