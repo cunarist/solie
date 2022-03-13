87754d9d5dce052a0b175c7cfae8efda597a9e37
temeddix <temeddix@gmail.com>
2022-03-13 14:34:15 +0900

    Fix the bug that removes timezone from dataframe

--- module/recipe/combine_candle_datas.py
+++ module/recipe/combine_candle_datas.py
@@ -5,6 +5,6 @@ def do(prior_df, secondary_df):
     df = prior_df.combine_first(secondary_df)
     df = df.sort_index(axis="index")
     df = df.sort_index(axis="columns")
-    df = df.resample("10S").asfreq()
+    df = df.asfreq("10S")
     df = df.astype(np.float32)
     return df
