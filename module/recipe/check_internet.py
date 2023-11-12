e30558db1753eb3a3091ba4045284908f5672c52
DongHyun Kim <temeddix@gmail.com>
2023-11-13 01:29:28 +0900

    Apply async system for internet connection check

--- module/recipe/check_internet.py
+++ module/recipe/check_internet.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
-import socket
+
+import aiohttp
 
 is_ready = asyncio.Event()
 _was_connected = False
@@ -19,7 +20,7 @@ async def monitor():
     global _was_connected
     while True:
         # try to connect to DNS servers
-        attempt_ips = (
+        attempt_ips = [
             "1.0.0.1",  # Cloudflare
             "1.1.1.1",  # Cloudflare
             "8.8.4.4",  # Google
@@ -28,15 +29,18 @@ async def monitor():
             "149.112.112.112",  # Quad9
             "208.67.222.222",  # OpenDNS
             "208.67.220.220",  # OpenDNS
-        )
+        ]
         is_connected = False
-        for attempt_ip in attempt_ips:
-            try:
-                socket.create_connection((attempt_ip, 53))
-                is_connected = True
-                break
-            except OSError:
-                pass
+        async with aiohttp.ClientSession() as session:
+            for attempt_ip in attempt_ips:
+                try:
+                    async with session.get(f"http://{attempt_ip}") as response:
+                        if response.status == 200:
+                            is_connected = True
+                            break
+                except aiohttp.ClientError:
+                    pass
+
         # detect changes
         if _was_connected and not is_connected:
             for job in _disconnected_functions:
@@ -46,9 +50,11 @@ async def monitor():
             for job in _connected_functions:
                 asyncio.create_task(job())
             logging.getLogger("solie").info("Internet connected")
+
         # remember connection state
         _was_connected = is_connected
         is_ready.set()
+
         # wait for a while
         await asyncio.sleep(1)
 
