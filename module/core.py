12ff769f792977654437ed0a8417b980ac6ee9cd
DongHyun Kim <temeddix@gmail.com>
2023-11-13 23:33:18 +0900

    Increase max instance of cron job

--- module/core.py
+++ module/core.py
@@ -129,7 +129,12 @@ class Window(QtWidgets.QMainWindow, Ui_MainWindow):
 
         self.initialize_functions: list[Callable[..., Coroutine]] = []
         self.finalize_functions: list[Callable[..., Coroutine]] = []
-        self.scheduler = AsyncIOScheduler(timezone="UTC")
+        self.scheduler = AsyncIOScheduler(
+            timezone="UTC",
+            job_defaults={
+                "max_instances": 2,
+            },
+        )
 
         # ■■■■■ do basic Qt things ■■■■■
 
