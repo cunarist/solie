f316a56cc84446c6f0026f424e94077ed981d57d
DongHyun Kim <temeddix@gmail.com>
2023-11-15 17:33:57 +0900

    Improve logging

--- module/instrument/log_handler.py
+++ module/instrument/log_handler.py
@@ -24,12 +24,17 @@ class LogHandler(logging.Handler):
         self.setFormatter(log_formatter)
 
         os.makedirs("./logs", exist_ok=True)
+        now = self.executed_time
+        self.filepath = (
+            f"./logs/{now.year:04}-{now.month:02}-{now.day:02}"
+            + f".{now.hour:02}-{now.minute:02}-{now.second:02}.{now.tzinfo}.txt"
+        )
 
     def emit(self, log_record: logging.LogRecord):
         formatted = self.format(log_record)
 
-        # when this is from exception
         if log_record.exc_info is not None:
+            # when this is from an exception
             lines = formatted.split("\n")
             summarization = lines[0]
             log_content = "\n".join(lines[1:])
@@ -38,8 +43,8 @@ class LogHandler(logging.Handler):
                 return
             exc_type = exception.__name__
             summarization += f" - {exc_type}"
-        # when this is a normal log
         else:
+            # when this is a normal log
             summarization = formatted
             log_content = log_record.getMessage()
             first_line_content = log_content.split("\n")[0].strip()
@@ -79,15 +84,6 @@ class LogHandler(logging.Handler):
         core.window.listWidget.addItem(summarization, log_content)
 
         # save to file
-        task_start_time = datetime.now(timezone.utc)
-        filename = str(self.executed_time)
-        filename = filename.replace(":", "_")
-        filename = filename.replace(" ", "_")
-        filename = filename.replace("-", "_")
-        filename = filename.replace("+", "_")
-        filename = f"./logs/{filename}.txt"
-        async with aiofiles.open(filename, "a", encoding="utf8") as file:
+        async with aiofiles.open(self.filepath, "a", encoding="utf8") as file:
             await file.write(f"{summarization}\n")
             await file.write(f"{log_content}\n\n")
-        duration = datetime.now(timezone.utc) - task_start_time
-        duration = duration.total_seconds()
