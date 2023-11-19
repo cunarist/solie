e9f9c14dbfee33d4a23c3796a5dfcebb5d9c7fcd
DongHyun Kim <temeddix@gmail.com>
2023-11-19 18:21:54 +0900

    Make copying to clipboard easy

--- solie/overlay/long_text_view.py
+++ solie/overlay/long_text_view.py
@@ -1,4 +1,7 @@
+import pyperclip
 from PySide6 import QtCore, QtGui, QtWidgets
+
+from solie.recipe import outsource
 
 
 class LongTextView(QtWidgets.QWidget):
@@ -7,16 +10,31 @@ class LongTextView(QtWidgets.QWidget):
 
         super().__init__()
 
+        long_text = payload[0]
+
         # ■■■■■ full layout ■■■■■
 
-        full_layout = QtWidgets.QHBoxLayout(self)
+        full_layout = QtWidgets.QVBoxLayout(self)
         cards_layout = QtWidgets.QVBoxLayout()
         full_layout.addLayout(cards_layout)
 
-        label = QtWidgets.QLabel(payload[0])
+        label = QtWidgets.QLabel(long_text)
         fixed_width_font = QtGui.QFont("Source Code Pro", 9)
         label.setFont(fixed_width_font)
         label.setTextInteractionFlags(
             QtCore.Qt.TextInteractionFlag.TextSelectableByMouse
         )
         cards_layout.addWidget(label)
+
+        # Copy to clipboard button
+
+        async def job_cc(*args):
+            pyperclip.copy(long_text)
+
+        button = QtWidgets.QPushButton("Copy to clipboard")
+        outsource.do(button.clicked, job_cc)
+        button.setSizePolicy(
+            QtWidgets.QSizePolicy.Policy.Fixed,
+            QtWidgets.QSizePolicy.Policy.Fixed,
+        )
+        full_layout.addWidget(button)
