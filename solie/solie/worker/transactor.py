8d958db01558e04c27654b5ab606376cddffbc95
DongHyun Kim <temeddix@gmail.com>
2023-11-26 04:05:21 +0900

    Organize code

--- solie/solie/worker/transactor.py
+++ solie/solie/worker/transactor.py
@@ -244,22 +244,19 @@ class Transactor:
 
     async def organize_data(self, *args, **kwargs):
         async with self.unrealized_changes.write_lock as cell:
-            original_index = cell.data.index
-            unique_index = await go(original_index.drop_duplicates)
+            unique_index = await go(cell.data.index.drop_duplicates)
             cell.data = await go(cell.data.reindex, unique_index)
             cell.data = await go(sort_pandas.series, cell.data)
             cell.data = await go(cell.data.astype, np.float32)
 
         async with self.auto_order_record.write_lock as cell:
-            original_index = cell.data.index
-            unique_index = await go(original_index.drop_duplicates)
+            unique_index = await go(cell.data.index.drop_duplicates)
             cell.data = await go(cell.data.reindex, unique_index)
             cell.data = await go(sort_pandas.data_frame, cell.data)
             cell.data = cell.data.iloc[-(2**16) :].copy()
 
         async with self.asset_record.write_lock as cell:
-            original_index = cell.data.index
-            unique_index = await go(original_index.drop_duplicates)
+            unique_index = await go(cell.data.index.drop_duplicates)
             cell.data = await go(cell.data.reindex, unique_index)
             cell.data = await go(sort_pandas.data_frame, cell.data)
 
