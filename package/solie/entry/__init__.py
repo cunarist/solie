cdf0106f3de5680850c57e3f94efe2837ad1c310
temeddix <temeddix@gmail.com>
2024-04-03 22:11:18 +0900

    Move things around

---
+++ package/solie/entry/__init__.py
@@ -0,0 +1,3 @@
+from .bring_to_life import bring_to_life
+
+__all__ = ["bring_to_life"]
--- package/solie/worker/__init__.py
+++ package/solie/worker/__init__.py
@@ -1,5 +1,13 @@
-from .collector import *
-from .manager import *
-from .simulator import *
-from .strategist import *
-from .transactor import *
+from .collector import Collector
+from .manager import Manager
+from .simulator import Simulator
+from .strategist import Strategiest
+from .transactor import Transactor
+
+__all__ = [
+    "Collector",
+    "Manager",
+    "Simulator",
+    "Strategiest",
+    "Transactor",
+]
