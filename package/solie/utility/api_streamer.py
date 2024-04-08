d7c3e8443ae0f0128ff0a1fdf470d9e8ae29769b
temeddix <temeddix@gmail.com>
2024-04-08 22:24:26 +0900

    Organize code

--- package/solie/utility/api_streamer.py
+++ package/solie/utility/api_streamer.py
@@ -3,7 +3,7 @@ import json
 import logging
 from typing import Callable, Coroutine
 
-import aiohttp
+from aiohttp import ClientSession, WSMsgType
 
 logger = logging.getLogger(__name__)
 
@@ -22,7 +22,7 @@ class ApiStreamer:
     ):
         self._url = url
         self._handler = handler
-        self.session = aiohttp.ClientSession()
+        self.session = ClientSession()
 
         if url != "":
             asyncio.create_task(self._run_websocket())
@@ -33,31 +33,32 @@ class ApiStreamer:
     async def _run_websocket(self):
         while True:
             try:
-                async with self.session.ws_connect(self._url) as websocket:
-                    logger.info(f"Websocket connected: {self._url}")
-                    async for received_raw in websocket:
-                        if received_raw.type in (
-                            aiohttp.WSMsgType.CLOSED,
-                            aiohttp.WSMsgType.ERROR,
-                        ):
-                            logger.info(f"Websocket closed: {self._url}")
-                            break
-                        else:
-                            received = received_raw.json()
-
-                            def done_callback(task: asyncio.Task, received=received):
-                                error = task.exception()
-                                if error:
-                                    raise ApiStreamError(received) from error
-
-                            task = asyncio.create_task(self._handler(received))
-                            task.add_done_callback(done_callback)
-                    logger.info(f"Websocket stopped: {self._url}")
+                await self._keep_connection()
             except Exception as error:
                 # Handle errors that might occur due to network issues
                 logger.exception(f"Websocket error: {error}")
                 # Wait for a few seconds before attempting to reconnect
                 await asyncio.sleep(5)
+
+    async def _keep_connection(self):
+        async with self.session.ws_connect(self._url) as websocket:
+            logger.info(f"Websocket connected: {self._url}")
+            async for received_raw in websocket:
+                stop_types = (WSMsgType.CLOSED, WSMsgType.ERROR)
+                if received_raw.type in stop_types:
+                    logger.info(f"Websocket closed: {self._url}")
+                    break
+                else:
+                    received = received_raw.json()
+
+                    def done_callback(task: asyncio.Task, received=received):
+                        error = task.exception()
+                        if error:
+                            raise ApiStreamError(received) from error
+
+                    task = asyncio.create_task(self._handler(received))
+                    task.add_done_callback(done_callback)
+            logger.info(f"Websocket stopped: {self._url}")
 
     async def _close_self(self):
         await self.session.close()
