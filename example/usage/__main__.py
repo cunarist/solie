b8e86ba2e7cc219bd07b5647362a256ca185c08d
Donghyun Kim <temeddix@gmail.com>
2025-02-16 03:37:17 +0900

    Add a comment

--- example/usage/__main__.py
+++ example/usage/__main__.py
@@ -2,6 +2,7 @@ from solie import SolieConfig, bring_to_life
 
 from usage import create_solie_config
 
+# Remember to guard the entry point for proper multiprocessing.
 if __name__ == "__main__":
     # Optionally provide configuration.
     config: SolieConfig = create_solie_config()
