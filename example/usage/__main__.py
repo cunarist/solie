7f484b8f2a9988688d691aeaa5ea73429a768b19
Donghyun Kim <temeddix@gmail.com>
2025-02-28 01:38:21 +0900

    Organize example code

--- example/usage/__main__.py
+++ example/usage/__main__.py
@@ -6,6 +6,5 @@ from usage import create_solie_config
 if __name__ == "__main__":
     # Optionally provide configuration.
     config: SolieConfig = create_solie_config()
-
     # Run Solie and show the window.
     bring_to_life(config)
