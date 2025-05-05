3e31657a513b2057bc9a29343b7c9c6f82672e39
Danny Kim <temeddix@gmail.com>
2025-05-05 19:40:16 +0900

    Improve the performance of internet connection checker

--- package/solie/utility/check_internet.py
+++ package/solie/utility/check_internet.py
@@ -1,4 +1,4 @@
-from asyncio import Event, sleep
+from asyncio import sleep
 from collections.abc import Callable, Coroutine
 from logging import getLogger
 from typing import Any, ClassVar
@@ -20,52 +20,52 @@ ATTEMPT_IP = [
 
 class StatusHolder:
     is_connected: ClassVar[bool] = False
-    is_internet_checked: ClassVar[Event] = Event()
     connected_calls: ClassVar[list[Callable[[], Coroutine[None, None, Any]]]] = []
     disconnected_calls: ClassVar[list[Callable[[], Coroutine[None, None, Any]]]] = []
 
 
 def internet_connected():
-    if StatusHolder.is_internet_checked.is_set():
-        return StatusHolder.is_connected
-    else:
-        raise RuntimeError("Internet connection is not being monitored")
+    return StatusHolder.is_connected
 
 
 async def start_monitoring_internet():
+    # Ensure that internet connection is initially checked
+    # when this function returns.
+    await monitor_internet()
+    # Repeatedly monitor the internet status.
     spawn(keep_monitoring_internet())
-    await StatusHolder.is_internet_checked.wait()
 
 
 async def keep_monitoring_internet():
     while True:
-        # Try to connect to DNS servers and analyze internet connection
-        was_connected = StatusHolder.is_connected
-        analyzed = False
-        async with ClientSession() as session:
-            for attempt_ip in ATTEMPT_IP:
-                try:
-                    async with session.get(f"http://{attempt_ip}") as response:
-                        if response.status == 200:
-                            analyzed = True
-                            break
-                except Exception:
-                    pass
-        StatusHolder.is_connected = analyzed
-        StatusHolder.is_internet_checked.set()
-
-        # Detect changes
-        if was_connected and not StatusHolder.is_connected:
-            for job in StatusHolder.disconnected_calls:
-                spawn(job())
-            logger.warning("Internet disconnected")
-        elif not was_connected and StatusHolder.is_connected:
-            for job in StatusHolder.connected_calls:
-                spawn(job())
-            logger.info("Internet connected")
-
-        # Wait for a while
+        await monitor_internet()
         await sleep(1)
+
+
+async def monitor_internet():
+    # Try to connect to DNS servers and analyze internet connection.
+    was_connected = StatusHolder.is_connected
+    analyzed = False
+    async with ClientSession() as session:
+        for attempt_ip in ATTEMPT_IP:
+            try:
+                async with session.get(f"http://{attempt_ip}") as response:
+                    if response.status == 200:
+                        analyzed = True
+                        break
+            except Exception:
+                pass
+    StatusHolder.is_connected = analyzed
+
+    # Detect changes.
+    if was_connected and not StatusHolder.is_connected:
+        for job in StatusHolder.disconnected_calls:
+            spawn(job())
+        logger.warning("Internet disconnected")
+    elif not was_connected and StatusHolder.is_connected:
+        for job in StatusHolder.connected_calls:
+            spawn(job())
+        logger.info("Internet connected")
 
 
 def when_internet_connected(job: Callable[[], Coroutine[None, None, Any]]):
