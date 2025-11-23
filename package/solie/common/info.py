f3ce5fccf5ef6d32ad09ee4d1a674681538a48bc
Danny Kim <temeddix@gmail.com>
2025-11-24 03:17:10 +0900

    Add `PTH` Ruff lint rule

--- package/solie/common/info.py
+++ package/solie/common/info.py
@@ -1,8 +1,7 @@
-import os
 from importlib import import_module, metadata
 from inspect import getfile
 from pathlib import Path
 
 PACKAGE_NAME = __name__.split(".")[0]
 PACKAGE_VERSION = metadata.version(PACKAGE_NAME)
-PACKAGE_PATH = Path(os.path.dirname(getfile(import_module(PACKAGE_NAME))))
+PACKAGE_PATH = Path(getfile(import_module(PACKAGE_NAME))).parent
