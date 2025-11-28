606c9cef288568cf7f260ac4cd9047f2c5f7c49c
Danny Kim <temeddix@gmail.com>
2025-11-29 02:51:40 +0900

    Fix a subtle logic bug

--- package/solie/worker/collector.py
+++ package/solie/worker/collector.py
@@ -501,7 +501,7 @@ class Collector:
     async def wait_for_candle_data_ready(self, before_moment: datetime) -> bool:
         """Wait for candle data to be ready up to the specified moment.
 
-        Returns True when data is ready or after timeout.
+        Returns True when data is ready, False on timeout.
         """
         for _ in range(50):
             async with self.candle_data.read_lock as cell:
@@ -509,7 +509,7 @@ class Collector:
                 if last_index == before_moment:
                     return True
             await sleep(0.1)
-        return True
+        return False
 
     async def _open_binance_data_page(self) -> None:
         await spawn_blocking(webbrowser.open, "https://www.binance.com/en/landing/data")
